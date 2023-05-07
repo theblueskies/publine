7045986015379f8440e049d129aebc5ce61dcf51
theblueskies <druhin.bala@gmail.com>
2023-05-07 18:05:33 -0400

    iam updates

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -23,12 +23,54 @@ resource "aws_iam_role" "lambda_consumer_role" {
   }
 }
 
+resource "aws_iam_role" "ddb_cleaner_role" {
+  name = "ddb_cleaner_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name        = "ddb_cleaner_role"
+    Description = "lambda role to consume data from dynamodb streams"
+  }
+}
+
+
 resource "aws_iam_role_policy_attachment" "lambda_execution_policy" {
-    role = aws_iam_role.lambda_consumer_role.name
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole" 
+  role       = aws_iam_role.lambda_consumer_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_iam_role_policy_attachment" "ddbstream_execution_policy" {
-    role = aws_iam_role.lambda_consumer_role.name
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaDynamoDBExecutionRole" 
+  role       = aws_iam_role.lambda_consumer_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaDynamoDBExecutionRole"
+}
+
+resource "aws_iam_role_policy" "ddb_cleaner_policy" {
+  name = "ddb_cleaner_policy"
+  role = aws_iam_role.ddb_cleaner_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = ["dynamodb:*"],
+        Resource = aws_dynamodb_table.pubcoredb.arn
+      },
+    ]
+  })
 }
