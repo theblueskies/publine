3d1c8932b66a349b45c7602d6e6fbe1f2566804d
theblueskies <druhin.bala@gmail.com>
2023-05-07 18:29:36 -0400

    packages

--- lambdas/runner/go.mod
+++ lambdas/runner/go.mod
@@ -1,5 +1,5 @@
-module github.com/theblueskies/publine/lambdas
+module github.com/theblueskies/publine/lambdas/runner
 
 go 1.19
 
-require github.com/aws/aws-sdk-go-v2 v1.18.0 // indirect
+require github.com/aws/aws-lambda-go v1.41.0 // indirect
--- lambdas/runner/main.go
+++ lambdas/runner/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
 
-func main(){
-	fmt.Println("hello world")
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+        Name string `json:"name"`
+}
+
+func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+        return fmt.Sprintf("Hello %s!", name.Name ), nil
+}
+
+func main() {
+        lambda.Start(HandleRequest)
 }
